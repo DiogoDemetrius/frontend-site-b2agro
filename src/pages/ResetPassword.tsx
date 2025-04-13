3a45b8b256b1fc4b58ae9d47a8f1ebf5b316aa7b
Diogo Demetrius <diogod.mmoreira@gmail.com>
2025-04-13 15:35:38 -0300

    correction dep

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FaLock } from 'react-icons/fa';
 
 const ResetPassword = () => {
